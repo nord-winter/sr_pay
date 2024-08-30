0448db3f236bdf17d2333d7bb016046dde6b3bff
JamieJamie <alone.sosad500@gmail.com>
2024-08-30 16:38:12 +0700

    Change color

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,4 @@
+  
   <footer id="footer" class="footer dark-background">
 
     <div class="container footer-top">
@@ -57,14 +58,22 @@
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Bootslander</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
+      <p>FDA : 11-1-18157-1-0235</p>
+      <p>Copyright © 2567 All rights reserved.</p>
+      <p>
+        สงวนลิขสิทธิ์ ห้ามทำซ้ำ แจกจ่าย หรือส่งในรูปแบบใด ๆ หรือด้วยวิธีการใด ๆ รวมถึงการทำสำเนา บันทึก หรือวิธีการทางอิเล็กทรอนิกส์หรือเชิงกลอื่น ๆ
+        โดยไม่ได้รับอนุญาตเป็นลายลักษณ์อักษรจากผู้เผยแพร่ ยกเว้นในกรณีที่มีการอ้างอิงโดยย่อในรีวิวสำคัญและการใช้งานที่ไม่ใช่เชิงพาณิชย์อื่น ๆ ที่ได้รับอนุญาตตามกฎหมายลิขสิทธิ์
+      </p>
+      <p style="font-size: 11px; font-style: italic;">ควรกินอาหารหลากหลายครบ 5 หมู่ ในสัดส่วนที่เหมาะสมเป็นประจํา</p>
+      <p style="font-size: 11px; font-style: italic;">ไม่มีผลในการป้องกันหรือรักษาโรค | อ่านคําเตือนในฉลากก่อนบริโภค</p>
+      <p style="font-size: 11px; font-style: italic;">เด็กและสตรีมีครรภ์ไม่ควรรับประทาน | ผลลัพธ์ที่ได้ขึ้นอยู่กับสภาพร่างกายของแต่ละบุคคล</p>
+      <!-- <div class="credits"> -->
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you've purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+        <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+      <!-- </div> -->
     </div>
 
   </footer>
